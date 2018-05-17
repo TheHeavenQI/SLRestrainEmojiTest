64b5fa387b93cc30b06e4f9a46a7babeedf752d2
luhongyu <luhongyu@magic-beans.cn>
2018-05-17 10:41:15 +0800

    new

---
+++ SLRestrainEmoji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SLRestrainEmoji
+//
+//  Created by MagicBeans2 on 2018/5/17.
+//  Copyright © 2018年 Sloan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
